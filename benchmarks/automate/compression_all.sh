46ccdf354182bbdebfd3efd5c6cbbbd4044f9204
Shehbaz Jaffer <shehbazjaffer007@gmail.com>
2020-08-23 09:59:30 -0400

    add wrapper script for filesize and cpulimit

--- benchmarks/automate/compression_all.sh
+++ benchmarks/automate/compression_all.sh
@@ -1,3 +1,15 @@
+#./vm_ops.sh benchmark_config/compressionbm/cpulimit/compression_60.percent_nsc
+#FILES=`ls benchmark_config/compressionbm/filesize/*`
+FILES=`ls benchmark_config/compressionbm/cpulimit/*`
+for f in $FILES;
+do
+	echo "F - $f"
+#	echo $f
+	./vm_ops.sh "$f"
+done
+
+
+#FILES=`ls benchmark_config/compressionbm/filesize/*`
 FILES=`ls benchmark_config/compressionbm/filesize/*`
 for f in $FILES;
 do
